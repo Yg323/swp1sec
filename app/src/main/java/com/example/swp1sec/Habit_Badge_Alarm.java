51b20ebefcca16c5abd86ad14b9554614d153209
parkjaeyoung <wodud7950@naver.com>
2020-06-14 02:30:04 +0900

    뱃지,캘린더 수정삭제

---
+++ app/src/main/java/com/example/swp1sec/Habit_Badge_Alarm.java
@@ -0,0 +1,21 @@
+package com.example.swp1sec;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+import android.widget.Toast;
+
+
+public class Habit_Badge_Alarm extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent){
+
+        ((badge_dialog)badge_dialog.mContext).habitsetAlarm();
+
+        Toast.makeText(context,"알람이 울립니다 ",Toast.LENGTH_SHORT).show();
+        Log.e("알람","알람입니다");
+    }
+
+
+}
---
+++ app/src/main/java/com/example/swp1sec/TodoHabit_Badge_Alarm.java
@@ -0,0 +1,23 @@
+package com.example.swp1sec;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+import android.widget.Toast;
+
+
+public class TodoHabit_Badge_Alarm extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent){
+
+        ((badge_dialog)badge_dialog.mContext).todohabitsetAlarm();
+
+
+
+        Toast.makeText(context,"알람이 울립니다 ",Toast.LENGTH_SHORT).show();
+        Log.e("알람","알람입니다");
+    }
+
+
+}
